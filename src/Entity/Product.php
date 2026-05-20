0f3cd8b72e6fb76eb540918fd79001e76bdfd366
Souri-Dev <arielbensing1@gmail.com>
2026-05-20 10:50:59 +0800

    Added CRUD

---
+++ src/Entity/Product.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use BcMath\Number;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::NUMBER)]
+    private ?Number $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?Number
+    {
+        return $this->price;
+    }
+
+    public function setPrice(Number $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
